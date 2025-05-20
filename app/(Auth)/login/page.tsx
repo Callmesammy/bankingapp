3efe34e648fce4201c85ae790daaf68701fb4515
Callmesammy <callmesammy@yahoo.com>
2025-05-20 13:45:12 +0100

    update

--- app/(Auth)/login/page.tsx
+++ app/(Auth)/login/page.tsx
@@ -1,7 +1,15 @@
+import Image from "next/image";
+
 const Auth = () => {
     return ( 
-        <div>
-            Auth 
+        <div className="w-full h-full flex items-center justify-center">
+          <div className="grid lg:grid-cols-2 w-full h-full items-center justify-center">
+            Auth
+         <div className="w-full hidden lg:flex h-full bg-yellow-400 relative">
+            <Image src="/img.webp" alt="image" fill className="object-cover"/>
+         </div>
+          </div>
+             
         </div>
      );
 }
