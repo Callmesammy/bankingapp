29053c07c63ca0f5553917c61bbd7cffd64d2944
Callmesammy <callmesammy@yahoo.com>
2025-05-21 13:05:55 +0100

    update

--- app/(Auth)/login/_components/signup.tsx
+++ app/(Auth)/login/_components/signup.tsx
@@ -1,10 +1,10 @@
-"use client"
- 
-import { z } from "zod"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
+"use client";
 
-import { Button } from "@/components/ui/button"
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
@@ -13,153 +13,208 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-import { useState } from "react"
-import { Loader2 } from "lucide-react"
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import { Loader2 } from "lucide-react";
 
 const formSchema = z.object({
   email: z.string().min(2, {
-    message: "Enter your emails"
+    message: "Enter your emails",
   }),
   password: z.string().min(6, {
-    message: "enter correct password"
+    message: "enter correct password",
+  }),
+  state: z.string().min(2,{
+    message: "Enter state of residence"
+  }),
+  name: z.string().min(3, {
+    message: "Enter your full Name"
+  }), 
+  surname: z.string().min(3,{
+    message: "Enter surname"
+  }), 
+  address: z.string().min(8, {
+    message: "Enter full address"
+  }), 
+  code: z.coerce.number().min(3,{
+    message: "enter appropriate postal code"
   })
-})
 
+});
 
 const Signup = () => {
-  const [isLoading, setIsLoading] = useState(false)
-      // 1. Define your form.
+  const [isLoading, setIsLoading] = useState(false);
+  // 1. Define your form.
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       email: "",
     },
-  })
- 
+  });
+
   // 2. Define a submit handler.
   function onSubmit(values: z.infer<typeof formSchema>) {
-    setIsLoading(true)
-    try{
-
-    }catch(error){
-      console.error("something went wrong", error)
-    }finally{
-      setIsLoading(false)
+    setIsLoading(true);
+    try {
+    } catch (error) {
+      console.error("something went wrong", error);
+    } finally {
+      setIsLoading(false);
     }
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
-    console.log(values)
+    console.log(values);
   }
 
-    return (
-        <div className="pt-3 ">
- <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-      <div className="w-full grid grid-cols-2 gap-3  ">
+  return (
+    <div className="pt-3 ">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <div className="w-full grid grid-cols-2 gap-3  ">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>First Name*</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={isLoading}
+                      required
+                      placeholder="First Name"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="surname"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Last Name*</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={isLoading}
+                      required
+                      placeholder="Last name"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <FormField
+            control={form.control}
+            name="address"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Address*</FormLabel>
+                <FormControl>
+                  <Input
+                    disabled={isLoading}
+                    required
+                    placeholder="Enter Address"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="w-full grid grid-cols-2 gap-3  ">
+            <FormField
+              control={form.control}
+              name="state"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>State*</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={isLoading}
+                      required
+                      placeholder="eg. NY"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="code"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Postal Code*</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={isLoading}
+                      required
+                      placeholder="postal code"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
 
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>First Name</FormLabel>
-              <FormControl>
-                <Input disabled={isLoading} type="email" required placeholder="email" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-           <FormField
-          control={form.control}
-          name="password"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Last Name</FormLabel>
-              <FormControl>
-                <Input disabled={isLoading} required  placeholder="Last name" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-              </div>
-              <FormField
-          control={form.control}
-          name="password"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Address</FormLabel>
-              <FormControl>
-                <Input disabled={isLoading} required  placeholder="Enter Address" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
- <div className="w-full grid grid-cols-2 gap-3  ">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email*</FormLabel>
+                <FormControl>
+                  <Input
+                    disabled={isLoading}
+                    type="email"
+                    required
+                    placeholder="email"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-<FormField
-  control={form.control}
-  name="email"
-  render={({ field }) => (
-    <FormItem>
-      <FormLabel>State</FormLabel>
-      <FormControl>
-        <Input disabled={isLoading} type="email" required placeholder="email" {...field} />
-      </FormControl>
-      <FormMessage />
-    </FormItem>
-  )}
-/>
-   <FormField
-  control={form.control}
-  name="password"
-  render={({ field }) => (
-    <FormItem>
-      <FormLabel>Postal Code</FormLabel>
-      <FormControl>
-        <Input disabled={isLoading} required  placeholder="Last name" {...field} />
-      </FormControl>
-      <FormMessage />
-    </FormItem>
-  )}
-/>
-      </div>
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password*</FormLabel>
+                <FormControl>
+                  <Input
+                    disabled={isLoading}
+                    type="password"
+                    required
+                    placeholder="password"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button
+            type="submit"
+            className="w-full bg-blue-700 hover:bg-sky-600 cursor-pointer font-semibold"
+          >
+            Signup {isLoading && <Loader2 className="animate-spin size-5" />}
+          </Button>
+        </form>
+      </Form>{" "}
+    </div>
+  );
+};
 
-      <FormField
-  control={form.control}
-  name="email"
-  render={({ field }) => (
-    <FormItem>
-      <FormLabel>State</FormLabel>
-      <FormControl>
-        <Input disabled={isLoading} type="email" required placeholder="email" {...field} />
-      </FormControl>
-      <FormMessage />
-    </FormItem>
-  )}
-/>
-
-<FormField
-  control={form.control}
-  name="email"
-  render={({ field }) => (
-    <FormItem>
-      <FormLabel>State</FormLabel>
-      <FormControl>
-        <Input disabled={isLoading} type="email" required placeholder="email" {...field} />
-      </FormControl>
-      <FormMessage />
-    </FormItem>
-  )}
-/>
-        <Button type="submit" className="w-full bg-blue-700 hover:bg-sky-600 cursor-pointer font-semibold">Login {isLoading && <Loader2 className="animate-spin size-5"/>}</Button>
-      </form>
-    </Form>        </div>
-      );
-}
- 
 export default Signup;
--- app/(Auth)/login/page.tsx
+++ app/(Auth)/login/page.tsx
@@ -10,7 +10,7 @@ const Auth = () => {
     return ( 
         <div className="w-full h-full flex items-center justify-center overflow-auto">
           <div className="grid lg:grid-cols-2 w-full h-full items-center justify-center ">
-            <div className=" rounded  lg:px-4 w-[29rem]  mb-3 h-full flex flex-col justify-center">
+            <div className=" rounded  lg:px-10 w-full  mb-3 h-full flex flex-col justify-center">
        <span className="flex space-x-1"> <GrAd  className="size-7"/> <h1 className="font-semibold">JEtsY</h1> </span> 
            
            
@@ -20,7 +20,7 @@ const Auth = () => {
             <p className="font-semibold text-sm text-muted-foreground">
             {entered === "login"? "welcome back! Please enter your account":  "Please enter your details" }
             </p>  
-            {entered === "login" ? <div className="pt-4"><Login/><span className="justify-end flex w-full pt-3 text-sm gap-2"> Don't have an account ? {" "} <span onClick={()=>setEntered("/signup")} className="text-blue-300 hover:text-sky-800 cursor-pointer"> {" "} signup </span></span></div>: <Signup/>}
+            {entered === "login" ? <div className="pt-4"><Login/><span className="justify-end flex w-full pt-3 text-sm gap-2"> Don't have an account ? {" "} <span onClick={()=>setEntered("/signup")} className="text-blue-600 hover:text-sky-800 cursor-pointer font-semibold"> {" "} Signup </span></span></div>:<div><Signup/> <p className="w-full  pt-3 text-end text-sm">Already have an account? <span className="text-blue-600 font-semibold hover:text-sky-500 cursor-pointer" onClick={()=>setEntered("login")}>Login</span></p></div> }
  
             </div>
          <div className="w-full hidden lg:flex h-full relative">
